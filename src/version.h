c372f550091171ed75d4be1e908e7efeaca32e0f
ClassicBitcoins <admin@bitclassic.info>
2021-01-13 10:38:06 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 790009;
+static const int PROTOCOL_VERSION = 890009;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
